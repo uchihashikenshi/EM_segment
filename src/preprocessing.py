1a20395329d4ff31af24d5a0fb110f9c066ad585
uchihashi kenshi <uchi_k@hakuri.local>
2017-01-10 02:45:23 +0900

    #6 preprocessing完成

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -6,12 +6,6 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-from theano.tensor.signal import downsample
-import theano.tensor as T
-import theano
-
-mem_cGAN_home = os.getcwd() # projects/memCNNから走らせる予定
-
 # ✕train ◯training
 
 """
@@ -34,14 +28,14 @@ data---raw--------------------train-input
 
 class Preprocessing(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, tif_data_dir, mem_cgan_home=os.getcwd()):
+        self.tif_data_dir = tif_data_dir
+        self.mem_cgan_home = mem_cgan_home
 
-    @staticmethod
-    def load_tif_images(data_dir):
-        os.chdir("data/%s" % data_dir)
+    def load_tif_images(self, data_name=''):
+        os.chdir(self.tif_data_dir + '/' + data_name)
         files = glob.glob('*.tif')
-        os.chdir("%s" % mem_cGAN_home)
+        os.chdir(self.mem_cgan_home)
         return files
 
     @staticmethod
@@ -50,25 +44,25 @@ class Preprocessing(object):
         raw_matrix = np.array(list(raw_image.getdata())).reshape(1024, 1024)
         return raw_matrix
 
-    def make_median_extracted_dataset(self, data_type, data_dir):
-        filelist = self.load_tif_images(data_dir)
+    def make_median_extracted_dataset(self, data_type):
+        files = self.load_tif_images()
 
-        if os.path.exists("%s/data/preprocessed/%s/median_extract_%s_dataset" % (mem_cGAN_home, data_type, data_type)) != True:
-            os.mkdir("%s/data/preprocessed/%s/median_extract_%s_dataset" % (mem_cGAN_home, data_type, data_type)) # データ置き場用意
+        if os.path.exists("%s/data/preprocessed/%s/median_extract_%s_dataset" % (mem_cgan_home, data_type, data_type)) != True:
+            os.mkdir("%s/data/preprocessed/%s/median_extract_%s_dataset" % (mem_cgan_home, data_type, data_type)) # データ置き場用意
 
         # スタック中全画像からmedianを求める(medianの平均値)
         # fixme: medianの平均でいいのか・・・？
         N, _sum = 0, 0
-        for file in filelist:
-            raw_matrix = self.image_to_array("data/%s/%s" % (data_dir, file))
+        for _file in files:
+            raw_matrix = self.image_to_array("data/%s/%s" % (_file))
             median = np.median(raw_matrix)
             _sum += median
             N += 1
         stack_median = _sum / N
 
         file_num = 1
-        for file in filelist:
-            raw_matrix = self.image_to_array("data/%s/%s" % (data_dir, file))
+        for _file in files:
+            raw_matrix = self.image_to_array("data/%s/%s" % (_file))
             median = np.median(raw_matrix) #中央値
             # スタックのmedianに各画像のmedianを合わせる
             median_extract_matrix = (raw_matrix - (median - stack_median))
@@ -81,7 +75,7 @@ class Preprocessing(object):
                         median_extract_matrix[i][j] = 0
 
             median_extract_image = Image.fromarray(np.uint8(median_extract_matrix).reshape(1024, 1024))
-            median_extract_image.save("%s/data/preprocessed/%s/median_extract_%s_dataset/median_extract_image_%03d.tif" % (mem_cGAN_home, data_type, data_type, file_num))
+            median_extract_image.save("%s/data/preprocessed/%s/median_extract_%s_dataset/median_extract_image_%03d.tif" % (mem_cgan_home, data_type, data_type, file_num))
             file_num += 1
             if file_num % 10 == 0:
                 print "%s images ended" % file_num
@@ -90,8 +84,8 @@ class Preprocessing(object):
     def make_average_pooled_dataset(self, data_type, data_dir):
         filelist = self.load_tif_images(data_dir)
 
-        if os.path.exists("%s/data/preprocessed/%s/pooled_%s_dataset" % (mem_cGAN_home, data_type, data_type)) != True:
-            os.mkdir("%s/data/preprocessed/%s/pooled_%s_dataset" % (mem_cGAN_home, data_type, data_type)) # データ置き場用意
+        if os.path.exists("%s/data/preprocessed/%s/pooled_%s_dataset" % (mem_cgan_home, data_type, data_type)) != True:
+            os.mkdir("%s/data/preprocessed/%s/pooled_%s_dataset" % (mem_cgan_home, data_type, data_type)) # データ置き場用意
 
         file_num = 1
         for file in filelist:
@@ -106,97 +100,74 @@ class Preprocessing(object):
                     pooled_pixel = _sum / 16
                     pooled_matrix.append(pooled_pixel)
             pooled_image = Image.fromarray(np.uint8(pooled_matrix).reshape(256, 256))
-            pooled_image.save("%s/data/preprocessed/%s/pooled_%s_dataset/pooled_image_%03d.tif" % (mem_cGAN_home, data_type, data_type, file_num))
+            pooled_image.save("%s/data/preprocessed/%s/pooled_%s_dataset/pooled_image_%03d.tif" % (mem_cgan_home, data_type, data_type, file_num))
             file_num += 1
             if file_num % 10 == 0:
                 print "%s images ended" % file_num
         print "pooled_%s_dataset is created." % data_type
 
-    def patch_extract(self, data_dir, label_data_dir, prefix = "", image_size = 256, crop_size = 33, stride = 5):
+    def patch_extract(self, data_dir, label_data_dir, prefix='', image_size=512, crop_size=256, stride=16):
         """
         1 stackをtraining 80枚、test20枚に分ける
         """
-        filelist = self.load_tif_images(data_dir)
-        labellist = self.load_tif_images(label_data_dir)
+        files = self.load_tif_images(data_dir)
+        labels = self.load_tif_images(label_data_dir)
 
-        center = (crop_size - 1) / 2
-
-        # training dastaset作成
-        if os.path.exists("%s/data/training_dataset/%straining_dataset" % (mem_cGAN_home, prefix)) != True:
-            os.mkdir("%s/data/training_dataset/%straining_dataset" % (mem_cGAN_home, prefix)) # trainingデータ置き場用意
-        else:
-            # 既にdatasetが存在すればエラーを返す
-            print "%s/data/training_dataset/%straining_dataset already exist." % (mem_cGAN_home, prefix)
+        # train dataを置くディレクトリを作成
+        if os.path.exists("%s/data/preprocessed/%strain" % (self.mem_cgan_home, prefix)):
+            print "%s/data/preprocessed/%strain already exist." % (self.mem_cgan_home, prefix)
             return
-        # test dataset作成
-        if os.path.exists("%s/data/test_dataset/%stest_dataset" % (mem_cGAN_home, prefix)) != True:
-            os.mkdir("%s/data/test_dataset/%stest_dataset" % (mem_cGAN_home, prefix)) # testデータ置き場用意
         else:
-            # 既にdatasetが存在すればエラーを返す
-            print "%s/data/test_dataset/%stest_dataset already exist." % (mem_cGAN_home, prefix)
+            os.mkdir("%s/data/preprocessed/%strain" % (self.mem_cgan_home, prefix))
+            os.mkdir("%s/data/preprocessed/%strain/input" % (self.mem_cgan_home, prefix))
+            os.mkdir("%s/data/preprocessed/%strain/label" % (self.mem_cgan_home, prefix))
+
+        # test dataを置くディレクトリを作成
+        if os.path.exists("%s/data/preprocessed/%stest" % (self.mem_cgan_home, prefix)):
+            print "%s/data/preprocessed/%stest already exist." % (self.mem_cgan_home, prefix)
             return
-        # reconstruction test dataset作成
-        if os.path.exists("%s/data/test_dataset/reconstruction_%stest_dataset" % (mem_cGAN_home, prefix)) != True:
-            os.mkdir("%s/data/test_dataset/reconstruction_%stest_dataset" % (mem_cGAN_home, prefix)) # testデータ置き場用意
         else:
-            # 既にdatasetが存在すればエラーを返す
-            print "%s/data/test_dataset/reconstruction_%stest_dataset already exist." % (mem_cGAN_home, prefix)
-            return
+            os.mkdir("%s/data/preprocessed/%stest" % (self.mem_cgan_home, prefix))
+            os.mkdir("%s/data/preprocessed/%stest/input" % (self.mem_cgan_home, prefix))
+            os.mkdir("%s/data/preprocessed/%stest/label" % (self.mem_cgan_home, prefix))
 
-        # trainig, testのデータベース作成用txt(名前は全てtraining.txt, test.txt)
-        training_f = open("%s/data/training_dataset/%straining_dataset/training.txt" % (mem_cGAN_home, prefix), 'w')
-        test_f = open("%s/data/test_dataset/%stest_dataset/test.txt" % (mem_cGAN_home, prefix), 'w')
-        # reconstruction testのデータベース作成用txt(名前は全てtraining.txt, test.txt)
-        reconstruction_test_f = open("%s/data/test_dataset/reconstruction_%stest_dataset/test.txt" % (mem_cGAN_home, prefix), 'w')
-
-        file_index = 1
-        # 正例・負例の数をそれぞれ数える処理
-        ans_0_number, ans_1_number = (0, 0)
-        for file, label in zip(filelist, labellist):
+        file_index = 0
+        crop_num = int(((image_size - crop_size) / stride + 1)**2)
+        for _file, label in zip(files, labels):
+            file_index += 1
             # 縦横の切り取り回数を計算(適当)
-            for h in xrange(int((image_size - crop_size) / stride)):
+            for h in xrange(crop_num):
                 for w in xrange(int((image_size - crop_size) / stride)):
 
                     # 画像のサイズを指定
                     patch_range = (w * stride, h * stride, w * stride + crop_size, h * stride + crop_size)
-                    cropped_image = Image.open("%s/data/%s/%s" % (mem_cGAN_home, data_dir, file)).crop(patch_range)
-                    cropped_label = Image.open("%s/data/%s/%s" % (mem_cGAN_home, label_data_dir, label)).crop(patch_range)
-                    ans = int(np.array(list((cropped_label.getdata()))).reshape((crop_size, crop_size))[center][center] / 255)
+                    cropped_image = Image.open("%s/%s/%s" % (self.tif_data_dir, data_dir, _file)).crop(patch_range)
+                    cropped_label = Image.open("%s/%s/%s" % (self.tif_data_dir, label_data_dir, label)).crop(patch_range)
 
-                    if (ans_0_number >= ans_1_number and ans == 1) or (ans_0_number <= ans_1_number and ans == 0):
-                        #通常の保存処理
-                        # 保存部分
-                        if file_index <= 80:
-                            cropped_image.save("%s/data/training_dataset/%straining_dataset/%straining_image_%03d%03d%03d.tif" % (mem_cGAN_home, prefix, prefix, file_index, h, w))
-                            training_f.write("%straining_image_%03d%03d%03d.tif %s\n" % (prefix, file_index, h, w, ans))
-                        else:
-                            cropped_image.save("%s/data/test_dataset/%stest_dataset/%stest_image_%03d%03d%03d.tif" % (mem_cGAN_home, prefix, prefix, file_index, h, w))
-                            test_f.write("%stest_image_%03d%03d%03d.tif %s\n" % (prefix, file_index, h, w, ans))
-                        # 正例・負例を同数にする処理
-                        if ans == 0:
-                            ans_0_number += 1
-                        else:
-                            ans_1_number += 1
-
-            if file_index == 90:
-                for h in xrange(int(image_size - crop_size)):
-                    for w in xrange(int(image_size - crop_size)):
-                        # 画像のサイズを指定
-                        patch_range = (w, h, w + crop_size, h + crop_size)
-                        cropped_image = Image.open("%s/data/%s/%s" % (mem_cGAN_home, data_dir, file)).crop(patch_range)
-                        cropped_label = Image.open("%s/data/%s/%s" % (mem_cGAN_home, label_data_dir, label)).crop(patch_range)
-                        ans = int(np.array(list((cropped_label.getdata()))).reshape((crop_size, crop_size))[center][center] / 255)
-                        cropped_image.save("%s/data/test_dataset/reconstruction_%stest_dataset/reconstruction_%stest_image_%03d%03d%03d.tif" % (mem_cGAN_home, prefix, prefix, file_index, h, w))
-                        reconstruction_test_f.write("reconstruction_%stest_image_%03d%03d%03d.tif %s\n" % (prefix, file_index, h, w, ans))
+                    # 保存部分
+                    if file_index <= crop_num * 0.8:
+                        cropped_image.save("%s/data/preprocessed/%strain/input/image_%03d%03d%03d.jpg" % (self.mem_cgan_home, prefix, file_index, h, w))
+                        cropped_label.save("%s/data/preprocessed/%strain/label/label_%03d%03d%03d.jpg" % (self.mem_cgan_home, prefix, file_index, h, w))
+                    else:
+                        cropped_image.save("%s/data/preprocessed/%stest/input/image_%03d%03d%03d.jpg" % (self.mem_cgan_home, prefix, file_index, h, w))
+                        cropped_label.save("%s/data/preprocessed/%stest/label/label_%03d%03d%03d.jpg" % (self.mem_cgan_home, prefix, file_index, h, w))
 
             if file_index % 10 == 0:
                 print "%s images ended" % file_index
 
-            if file_index == 80:
-                print "%straining_dataset is created." % prefix
-                #debug:
-                print ans_0_number
-                print ans_1_number
-            if file_index == 100:
-                print "%stest_dataset is created." % prefix
-            file_index += 1
+            # if file_index == crop_num * 0.8:
+            #     print "%straining_dataset is created." % prefix
+            #
+            # if file_index == 100:
+            #     print "%stest_dataset is created." % prefix
+
+if __name__ == '__main__':
+    try:
+        tif_data_dir = sys.argv[1]
+        preprocessing = Preprocessing(tif_data_dir=tif_data_dir)
+        preprocessing.patch_extract(data_dir='train/input', label_data_dir='train/label')
+    except IndexError:
+        quit()
+    else:
+        quit()
+
