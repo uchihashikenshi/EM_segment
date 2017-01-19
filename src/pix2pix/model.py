6de09b129ff71ddd611d5260a2b318399ade5923
uchihashikenshi <uchihashi.kenshi@gmail.com>
2017-01-19 13:45:53 +0900

    #6 modelファイル修正

--- src/pix2pix/model.py
+++ src/pix2pix/model.py
@@ -390,7 +390,7 @@ class Pix2pix(object):
     def test(self, args):
         """Test pix2pix"""
         tf.initialize_all_variables().run()
-        sample_files = glob('{}/slice_{:03d}*.jpg'.format(self.test_input_dir, self.test_input_dir_prefix))
+        sample_files = glob('{}/slice_{:03d}*.jpg'.format(self.test_input_dir, int(self.test_input_dir_prefix)))
 
         # sort testing input
         # n = [int(i) for i in map(lambda x: x.split('/')[-1].split('.jpg')[0], sample_files)]
@@ -427,6 +427,6 @@ class Pix2pix(object):
                 feed_dict={self.real_data: sample_image}
             )
             print(samples.shape)
-            save_images(samples, [self.batch_size, 1],'{}/{}_{:04d}.png'.format(args.test_dir, self.test_input_dir_prefix, idx))
+            save_images(samples, [self.batch_size, 1],'{}/slice{}_{:04d}.png'.format(args.test_dir, self.test_input_dir_prefix, idx))
             # samples = Image.fromarray(np.uint8(samples).reshape(256, 256))
             # samples.save('{}/test_{:04d}.png'.format(args.test_dir, idx))
